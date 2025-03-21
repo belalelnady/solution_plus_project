7a3f15eaf707e65736a0003b0576add91caaf1f7
Salmaa-Hesham <salmahesham1114@gmail.com>
2025-03-21 23:00:55 +0200

    Deleted unnecessary files

--- application/dbModel/initializeDB.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "MySQL is up - Executing scripts..."
-mysql -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /bootstrap/dbModel/schema.sql
-
-mysql -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /bootstrap/dbModel/initialize.sql
-
-echo "Database initialized successfully."
--- application/src/server/db/protected/dbcon-dev.ts
+++
@@ -1,8 +0,0 @@
-import * as  mysql from 'mysql';
-export const pool = mysql.createPool({
-connectionLimit : 10,
-host : process.env.DB_HOST,
-user : process.env.DB_USER,
-password :  process.env.DB_PASSWORD,
-database :  process.env.DB_NAME,
-})
